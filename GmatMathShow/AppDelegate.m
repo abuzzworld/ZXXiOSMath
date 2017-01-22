31f867b122772e8d844ddbeb895cf3f2c52699e2
zxx.abuzzworld <zxx.abuzzworldgmail.com>
2017-01-22 17:44:24 +0800

    function wtf

--- GmatMathShow/AppDelegate.m
+++ GmatMathShow/AppDelegate.m
@@ -26,6 +26,10 @@
     return YES;
 }
 
+- (void)wft
+{
+    NSLog(@"%@", [NSString stringWithUTF8String:__func__]);
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
